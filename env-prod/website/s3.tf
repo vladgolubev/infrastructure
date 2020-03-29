9caf1a7badaeb96d8c56d098dea3ef1f05a49d1f
Vlad Holubiev <golubev.vld@gmail.com>
2020-03-29 19:29:06 +0300

    fix: make error doc the index.html as well

--- env-prod/website/s3.tf
+++ env-prod/website/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "vladholubiev_com" {
 
   website {
     index_document = "index.html"
-    error_document = "error.html"
+    error_document = "index.html"
   }
 
   lifecycle {
